bfcf2b8c8a2e365396db3b9ac6c7865128d1ea2c
sushnavi <sushmita.b.navi@ibm.com>
2025-05-05 13:29:08 +0530

    Add files via upload

---
+++ TestNG/Activities/activity4DemoOne.java
@@ -0,0 +1,17 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class activity4DemoOne {
+
+	@Test
+	public void firstTestCase() {
+		System.out.println("I'm in first test case from demoOne Class");
+	}
+
+	@Test
+	public void secondTestCase() {
+		System.out.println("I'm in second test case from demoOne Class");
+	}
+
+}
---
+++ TestNG/Activities/activity4DemoTwo.java
@@ -0,0 +1,12 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class activity4DemoTwo {
+
+	@Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+	
+}
